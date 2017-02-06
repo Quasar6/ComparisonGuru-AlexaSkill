4dcd299d133309398cdc3c262ba517d915925399
shelalaine <schan211@my.centennialcollege.ca>
2017-02-06 01:59:25 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,276 @@
+/**
+ * Team Name: Quasar
+ * Description:
+ * 
+ * - Web service: communicate with an external web service to get the best price of a product (Quasar API)
+ * - Pagination: after obtaining a list of events, read a small subset of events and wait for user prompt to read the next subset of events by maintaining session state
+ * - Dialog and Session state: Handles two models, both a one-shot ask and tell model, and a multi-turn dialog model.
+ * - SSML: Using SSML tags to control how Alexa renders the text-to-speech.
+ *
+ * Examples:
+ * One-shot model:
+ * User:  "Alexa, ask ComparisonGuru to get the cheapest of Google Pixel phone."
+ *
+ * Dialog model:
+ * User:  "Alexa, open ComparisonGuru"
+ * Alexa: "Welcome to Comparison Guru. What product you want to check?"
+ * 
+ * Change History: Feb 6, 2017 - Created 
+ */
+
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+
+var https = require('https');
+
+/**
+ * The AlexaSkill Module that has the AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+
+/**
+ * URL prefix to download history content from Quasar's backend
+ */
+var urlPrefix = 'https://cguru-quasar6.rhcloud.com/cheapest/bestbuy/';
+
+/**
+ * Variable defining number of events to be read at one time
+ */
+var paginationSize = 3;
+
+/**
+ * Variable defining the length of the delimiter between events
+ */
+var delimiterSize = 2;
+
+/**
+ * ComparisonGuru is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var ComparisonGuruSkill = function() {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+ComparisonGuruSkill.prototype = Object.create(AlexaSkill.prototype);
+ComparisonGuruSkill.prototype.constructor = ComparisonGuruSkill;
+
+ComparisonGuruSkill.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("ComparisonGuruSkill onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+
+    // any session init logic would go here
+};
+
+ComparisonGuruSkill.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("ComparisonGuruSkill onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    getWelcomeResponse(response);
+};
+
+ComparisonGuruSkill.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+
+    // any session cleanup logic would go here
+};
+
+ComparisonGuruSkill.prototype.intentHandlers = {
+
+    "GetFirstEventIntent": function (intent, session, response) {
+        handleFirstEventRequest(intent, session, response);
+    },
+
+    "GetNextEventIntent": function (intent, session, response) {
+        handleNextEventRequest(intent, session, response);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "With Comparison Guru, you can compare product prices across major online shopping stores in US and Canada. Now, what product you want to check?";
+        var repromptText = "What product you want to check?";
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        var repromptOutput = {
+            speech: repromptText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.ask(speechOutput, repromptOutput);
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = {
+                speech: "Goodbye",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = {
+                speech: "Goodbye",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.tell(speechOutput);
+    }
+};
+
+/**
+ * Function to handle the onLaunch skill behavior
+ */
+
+function getWelcomeResponse(response) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    var cardTitle = "Price Comparison Guru";
+    var repromptText = "<p>With Comparison Guru, you can compare product prices across major online shopping stores in Canada.</p> <p>Now, what product you want to check?</p>";
+    var speechText = "<p>Welcome to Comparison Guru.</p> <p>What product you want to check?</p>";
+    var cardOutput = "Comparison Guru. Name the product that you want to price check.";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+
+    var speechOutput = {
+        speech: "<speak>" + speechText + "</speak>",
+        type: AlexaSkill.speechOutputType.SSML
+    };
+    var repromptOutput = {
+        speech: "<speak>" + repromptText + "</speak>",
+        type: AlexaSkill.speechOutputType.SSML
+    };
+    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardOutput);
+}
+
+/**
+ * Gets a poster prepares the speech to reply to the user.
+ */
+function handleFirstEventRequest(intent, session, response) {
+    var productSlot = intent.slots.product;
+    var productSlotValue = (productSlot.value);
+
+    // Get the product to search 
+    var productToSearch = productSlotValue;
+    if (productSlotValue.startsWith('price of ')) {
+        productToSearch = productSlotValue.substring('price of '.length);
+    }
+    
+    var daySlot = intent.slots.product;
+    var repromptText = "With Comparison Guru, you can compare product prices across major online shopping stores in US and Canada.";
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+                      "July", "August", "September", "October", "November", "December"
+    ];
+    var sessionAttributes = {};
+    // Read the first 3 events, then set the count to 3
+    sessionAttributes.index = paginationSize;
+    var date = "";
+
+    date = new Date();
+
+    var prefixContent = "<p>The best price of " + productToSearch + " is </p>";
+    var cardContent = "Price of " + productToSearch;
+    var cardTitle = "Price of " + productToSearch;
+
+    fetchDataFromQuasar(productToSearch, function (events) {
+        var speechText = "",
+            i;
+        sessionAttributes.text = events;
+        session.attributes = sessionAttributes;
+        if (events.length == 0) {
+            speechText = "There is a problem connecting to Comparison Guru at this time. Please try again later.";
+            cardContent = speechText;
+            response.tell(speechText);
+        } else {
+            speechText = events + "<p>You can buy this in Best Buy.</p><p>Do you want to hear the next best price?</p>";
+            var speechOutput = {
+                speech: "<speak>" + prefixContent + speechText + "</speak>",
+                type: AlexaSkill.speechOutputType.SSML
+            };
+            var repromptOutput = {
+                speech: repromptText,
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+            };
+            response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
+        }
+    });
+}
+
+/**
+ * Gets a poster prepares the speech to reply to the user.
+ */
+function handleNextEventRequest(intent, session, response) {
+    var cardTitle = "More prices to check",
+        sessionAttributes = session.attributes,
+        result = sessionAttributes.text,
+        speechText = "",
+        cardContent = "",
+        repromptText = "Do you want to check the next best price of the product?",
+        i;
+    if (!result) {
+        speechText = "With Comparison Guru, you can compare product prices across major online shopping stores in US and Canada. Now, what product you want to check";
+        cardContent = speechText;
+    } else if (sessionAttributes.index >= result.length) {
+        speechText = "There are no more price information. Try to search for another product by saying <break time = \"0.3s\"/> get price of.";
+        cardContent = "There are no more price information. Try to search for another product by saying, get price of.";
+    } else {
+        for (i = 0; i < paginationSize; i++) {
+            if (sessionAttributes.index>= result.length) {
+                break;
+            }
+            speechText = speechText + "<p>" + result[sessionAttributes.index] + "</p> ";
+            cardContent = cardContent + result[sessionAttributes.index] + " ";
+            sessionAttributes.index++;
+        }
+        if (sessionAttributes.index < result.length) {
+            speechText = speechText + " Wanna check the next best price of the product?";
+            cardContent = cardContent + " Wanna check the next best price of the product?";
+        }
+    }
+    var speechOutput = {
+        speech: "<speak>" + speechText + "</speak>",
+        type: AlexaSkill.speechOutputType.SSML
+    };
+    var repromptOutput = {
+        speech: repromptText,
+        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+    };
+    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
+}
+
+function fetchDataFromQuasar(name, eventCallback) {
+    var url = urlPrefix + name;
+
+    https.get(url, function(res) {
+        var body = '';
+
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        res.on('end', function () {
+            var stringResult = parseJson(body);
+            eventCallback(stringResult);
+        });
+    }).on('error', function (e) {
+        console.log("Got error: ", e);
+    });
+}
+
+function parseJson(inputText) {
+    var text = "";
+    var total = inputText.substring(inputText.indexOf('"total\\": ')+10, inputText.indexOf(',\\n  \\"totalPages\\":'));
+    if (Number(total) > 0) {
+        text = inputText.substring(inputText.indexOf('"salePrice\\": ')+14, inputText.indexOf(',\\n      \\"manufacturer\\":'));
+    }
+    return text;
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the ComparisonGuru Skill.
+    var skill = new ComparisonGuruSkill();
+    skill.execute(event, context);
+};
+
